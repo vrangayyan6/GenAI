f79a288c59fc231f6768987254d7be7428155dec
vrangayyan6 <vrangayyan@hotmail.com>
2024-10-19 14:55:16 -0400

    Update openai-gradio_app.py
    updated with title, description and examples

--- openai-gradio_app.py
+++ openai-gradio_app.py
@@ -11,4 +11,7 @@ import openai_gradio
 gr.load(
     name='gpt-4o-mini',   # select model as needed
     src=openai_gradio.registry,
+    title='OpenAI-Gradio Integration',
+    description="Chat with GPT-4o-mini model.",
+    examples=["Self-ask technique: I want you to answer my questions by performing a series of steps involving asking sub-questions as you proceed to derive a final answer. You are to identify relevant sub-questions one at a time, answer each such sub-question one at a time, and then use those series of answers to reach a final overall answer. Do you understand these instructions on how I want you to solve problems?", "Explain quantum gravity to a 5-year old.", "How many R are there in the word Strawberry?"]
 ).launch()
